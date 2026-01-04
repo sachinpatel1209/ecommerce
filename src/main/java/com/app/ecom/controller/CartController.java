a796cdfc16d640da1136afa2dfbd11abdaccf80d
sachinpatel1209 <sachinpatel6720@gmail.com>
2026-01-04 17:25:41 +0530

    new api added like remove from cart and fetch cart

--- src/main/java/com/app/ecom/controller/CartController.java
+++ src/main/java/com/app/ecom/controller/CartController.java
@@ -1,11 +1,14 @@
 package com.app.ecom.controller;
 
 import com.app.ecom.dto.CartItemRequest;
+import com.app.ecom.model.CartItem;
 import com.app.ecom.service.CartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/cart")
@@ -21,5 +24,19 @@ public class CartController {
             return ResponseEntity.badRequest().body("Product out of the stock or user not found or Product not found ");
         }
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping("/items/{productId}")
+    public ResponseEntity<Void> removeFromCart(@RequestHeader("X-User-ID") String userId,
+                                           @PathVariable Long productId) {
+        boolean deleted = cartService.deleteItemFromCart(userId, productId);
+        return deleted ? ResponseEntity.noContent().build()
+                : ResponseEntity.notFound().build();
+    }
+
+    // fetch cart of the particular user
+    @GetMapping
+    public ResponseEntity<List<CartItem>> getCart(@RequestHeader("X-User-ID") String userId) {
+        return ResponseEntity.ok(cartService.getCart(userId));
     }
 }
--- src/main/java/com/app/ecom/service/CartService.java
+++ src/main/java/com/app/ecom/service/CartService.java
@@ -7,19 +7,22 @@ import com.app.ecom.model.User;
 import com.app.ecom.repository.CartItemRepository;
 import com.app.ecom.repository.ProductRepository;
 import com.app.ecom.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class CartService {
-    
+
     private final CartItemRepository itemRepository;
 
-    private final ProductRepository  productRepository;
+    private final ProductRepository productRepository;
 
     private final UserRepository userRepository;
 
@@ -30,12 +33,12 @@ public class CartService {
         if (productOpt.isEmpty()) {
             return false;
         }
-        Product  product = productOpt.get();
+        Product product = productOpt.get();
         if (product.getStockQuantity() < request.getQuantity()) {
             return false;
         }
         Optional<User> userOpt = userRepository.findById(Long.valueOf(userId));
-        if (userOpt.isEmpty()){
+        if (userOpt.isEmpty()) {
             return false;
         }
         User user = userOpt.get();
@@ -48,7 +51,7 @@ public class CartService {
             itemRepository.save(existingCartItem);
         } else {
             // create the new cart
-            CartItem  cartItem = new CartItem();
+            CartItem cartItem = new CartItem();
             cartItem.setUser(user);
             cartItem.setProduct(product);
             cartItem.setQuantity(request.getQuantity());
@@ -56,6 +59,23 @@ public class CartService {
             itemRepository.save(cartItem);
         }
         return true;
+    }
+
+    public boolean deleteItemFromCart(String userId, Long productId) {
+
+        Optional<Product> productOpt = productRepository.findById(productId);
+        Optional<User> userOpt = userRepository.findById(Long.valueOf(userId));
+        if (productOpt.isPresent() && userOpt.isPresent()) {
+            itemRepository.deleteByUserAndProduct(userOpt.get(),productOpt.get());
+            return true;
+        }
+        return false;
+    }
+
+    public List<CartItem> getCart(String userId) {
+         return userRepository.findById(Long.valueOf(userId))
+                .map(itemRepository:: findByUser)
+                .orElseGet(List::of);
     }
 }
 
